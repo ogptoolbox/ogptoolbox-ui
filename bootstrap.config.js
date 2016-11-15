607cfba825ecbb1f17f0cc379086e15bea98368b
Christophe Benz <christophe.benz@gmail.com>
2016-11-15 12:01:37 +0100

    Use webpack extract text plugin in bootstrap config

--- bootstrap.config.js
+++ bootstrap.config.js
@@ -1,8 +1,7 @@
 module.exports = {
 
-  // Default for the style loading
-  styleLoader: 'style-loader!css-loader!less-loader',
-    
+  styleLoader: require('extract-text-webpack-plugin').extract('style-loader', 'css-loader!postcss-loader!less-loader'),
+
   scripts: {
     'transition': true,
     'alert': true,
