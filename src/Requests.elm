0f2bd3d2d29dbcb3afb3077a72b894e4a5907edb
Christophe Benz <christophe.benz@gmail.com>
2016-11-22 10:25:15 +0100

    Remove unused code

--- src/Requests.elm
+++ src/Requests.elm
@@ -3,9 +3,7 @@ module Requests exposing (..)
 import Authenticator.Model
 import Configuration exposing (apiUrl)
 import Decoders exposing (..)
-import Dict exposing (Dict)
 import Http
-import Json.Encode
 import String
 import Types exposing (..)
 import Task exposing (Task)
@@ -26,120 +24,6 @@ cardTypesForTool =
     [ "Software", "Platform" ]
 
 
-newTaskCreateStatement : Authenticator.Model.Authentication -> StatementCustom -> Task Http.Error DataIdBody
-newTaskCreateStatement authentication statementCustom =
-    let
-        bodyJson =
-            Json.Encode.object
-                ([ ( "type", Json.Encode.string (convertStatementCustomToKind statementCustom) ) ]
-                    ++ case statementCustom of
-                        AbuseCustom abuse ->
-                            [ ( "statementId", Json.Encode.string abuse.statementId )
-                            ]
-
-                        ArgumentCustom argument ->
-                            [ ( "argumentType", Json.Encode.string (convertArgumentTypeToString argument.argumentType) )
-                            , ( "claimId", Json.Encode.string argument.claimId )
-                            , ( "groundId", Json.Encode.string argument.groundId )
-                            ]
-
-                        CardCustom card ->
-                            Debug.crash "TODO"
-
-                        PlainCustom plain ->
-                            [ ( "languageCode", Json.Encode.string plain.languageCode )
-                            , ( "name", Json.Encode.string plain.name )
-                            ]
-
-                        TagCustom tag ->
-                            [ ( "name", Json.Encode.string tag.name )
-                            ]
-                )
-    in
-        Http.fromJson dataIdBodyDecoder
-            (Http.send Http.defaultSettings
-                { verb = "POST"
-                , url =
-                    (apiUrl
-                        ++ "statements"
-                        ++ "?depth=1&show=abuse&show=author&show=ballot&show=grounds&show=properties&show=tags"
-                    )
-                , headers =
-                    [ ( "Accept", "application/json" )
-                    , ( "Content-Type", "application/json" )
-                    , ( "Retruco-API-Key", authentication.apiKey )
-                    ]
-                , body = Http.string (Json.Encode.encode 2 bodyJson)
-                }
-            )
-
-
-newTaskDeleteStatementRating : Authenticator.Model.Authentication -> String -> Task Http.Error DataIdBody
-newTaskDeleteStatementRating authentication statementId =
-    Http.fromJson dataIdBodyDecoder
-        (Http.send Http.defaultSettings
-            { verb = "DELETE"
-            , url =
-                (apiUrl
-                    ++ "statements/"
-                    ++ statementId
-                    ++ "/rating?depth=1&show=abuse&show=author&show=ballot&show=grounds&show=properties&show=tags"
-                )
-            , headers =
-                [ ( "Accept", "application/json" )
-                , ( "Retruco-API-Key", authentication.apiKey )
-                ]
-            , body = Http.empty
-            }
-        )
-
-
-newTaskFlagAbuse : Authenticator.Model.Authentication -> String -> Task Http.Error DataIdBody
-newTaskFlagAbuse authentication statementId =
-    Http.fromJson dataIdBodyDecoder
-        (Http.send Http.defaultSettings
-            { verb = "GET"
-            , url =
-                (apiUrl
-                    ++ "statements/"
-                    ++ statementId
-                    ++ "/abuse?depth=1&show=abuse&show=author&show=ballot&show=grounds&show=properties&show=tags"
-                )
-            , headers =
-                [ ( "Accept", "application/json" )
-                , ( "Retruco-API-Key", authentication.apiKey )
-                ]
-            , body = Http.empty
-            }
-        )
-
-
-newTaskGetCard : Maybe Authenticator.Model.Authentication -> String -> Task Http.Error DataIdBody
-newTaskGetCard authenticationMaybe statementId =
-    let
-        authenticationHeaders =
-            case authenticationMaybe of
-                Just authentication ->
-                    [ ( "Retruco-API-Key", authentication.apiKey )
-                    ]
-
-                Nothing ->
-                    []
-    in
-        Http.fromJson dataIdBodyDecoder
-            (Http.send Http.defaultSettings
-                { verb = "GET"
-                , url =
-                    apiUrl ++ "statements/" ++ statementId
-                , headers =
-                    [ ( "Accept", "application/json" )
-                    ]
-                        ++ authenticationHeaders
-                , body = Http.empty
-                }
-            )
-
-
 newTaskGetCardOfType : Maybe Authenticator.Model.Authentication -> List String -> String -> Task Http.Error Statement
 newTaskGetCardOfType authenticationMaybe cardTypes statementId =
     let
@@ -157,32 +41,6 @@ newTaskGetCardOfType authenticationMaybe cardTypes statementId =
             (Http.send Http.defaultSettings
                 { verb = "GET"
                 , url = apiUrl ++ "statements/" ++ statementId
-                , headers =
-                    [ ( "Accept", "application/json" )
-                    ]
-                        ++ authenticationHeaders
-                , body = Http.empty
-                }
-            )
-
-
-newTaskGetCards : Maybe Authenticator.Model.Authentication -> Task Http.Error DataIdsBody
-newTaskGetCards authenticationMaybe =
-    let
-        authenticationHeaders =
-            case authenticationMaybe of
-                Just authentication ->
-                    [ ( "Retruco-API-Key", authentication.apiKey )
-                    ]
-
-                Nothing ->
-                    []
-    in
-        Http.fromJson dataIdsBodyDecoder
-            (Http.send Http.defaultSettings
-                { verb = "GET"
-                , url =
-                    apiUrl ++ "statements?type=Card"
                 , headers =
                     [ ( "Accept", "application/json" )
                     ]
@@ -249,35 +107,6 @@ newTaskGetOrganizations authenticationMaybe searchQuery =
     newTaskGetCardsOfType authenticationMaybe cardTypesForOrganization searchQuery
 
 
-newTaskGetStatements : Maybe Authenticator.Model.Authentication -> Task Http.Error DataIdsBody
-newTaskGetStatements authenticationMaybe =
-    let
-        authenticationHeaders =
-            case authenticationMaybe of
-                Just authentication ->
-                    [ ( "Retruco-API-Key", authentication.apiKey )
-                    ]
-
-                Nothing ->
-                    []
-    in
-        Http.fromJson dataIdsBodyDecoder
-            (Http.send Http.defaultSettings
-                { verb = "GET"
-                , url =
-                    (apiUrl
-                        ++ "statements"
-                        ++ "?depth=1&show=abuse&show=author&show=ballot&show=grounds&show=properties&show=tags"
-                    )
-                , headers =
-                    [ ( "Accept", "application/json" )
-                    ]
-                        ++ authenticationHeaders
-                , body = Http.empty
-                }
-            )
-
-
 newTaskGetTool : Maybe Authenticator.Model.Authentication -> String -> Task Http.Error Statement
 newTaskGetTool authenticationMaybe statementId =
     newTaskGetCardOfType authenticationMaybe cardTypesForTool statementId
@@ -286,80 +115,3 @@ newTaskGetTool authenticationMaybe statementId =
 newTaskGetTools : Maybe Authenticator.Model.Authentication -> String -> Task Http.Error (List Statement)
 newTaskGetTools authenticationMaybe searchQuery =
     newTaskGetCardsOfType authenticationMaybe cardTypesForTool searchQuery
-
-
-newTaskRateStatement : Authenticator.Model.Authentication -> Int -> String -> Task Http.Error DataIdBody
-newTaskRateStatement authentication rating statementId =
-    let
-        bodyJson =
-            Json.Encode.object
-                [ ( "rating", Json.Encode.int rating ) ]
-    in
-        Http.fromJson dataIdBodyDecoder
-            (Http.send Http.defaultSettings
-                { verb = "POST"
-                , url =
-                    (apiUrl
-                        ++ "statements/"
-                        ++ statementId
-                        ++ "/rating?depth=1&show=abuse&show=author&show=ballot&show=grounds&show=properties&show=tags"
-                    )
-                , headers =
-                    [ ( "Accept", "application/json" )
-                    , ( "Content-Type", "application/json" )
-                    , ( "Retruco-API-Key", authentication.apiKey )
-                    ]
-                , body = Http.string (Json.Encode.encode 2 bodyJson)
-                }
-            )
-
-
-updateFromDataId : DataId -> ModelFragment a -> ModelFragment a
-updateFromDataId data model =
-    { model
-        | ballotById =
-            Dict.merge
-                (\id ballot ballotById ->
-                    if ballot.deleted then
-                        ballotById
-                    else
-                        Dict.insert id ballot ballotById
-                )
-                (\id leftBallot rightBallot ballotById ->
-                    if leftBallot.deleted then
-                        ballotById
-                    else
-                        Dict.insert id leftBallot ballotById
-                )
-                Dict.insert
-                data.ballots
-                model.ballotById
-                Dict.empty
-        , statementById =
-            Dict.merge
-                (\id statement statementById ->
-                    if statement.deleted then
-                        statementById
-                    else
-                        Dict.insert id statement statementById
-                )
-                (\id leftStatement rightStatement statementById ->
-                    if leftStatement.deleted then
-                        statementById
-                    else
-                        Dict.insert id leftStatement statementById
-                )
-                Dict.insert
-                data.statements
-                model.statementById
-                Dict.empty
-        , statementIds =
-            if Dict.member data.id data.statements then
-                if List.member data.id model.statementIds then
-                    model.statementIds
-                else
-                    data.id :: model.statementIds
-            else
-                -- data.id is not the ID of a statement (but a ballot ID, etc).
-                model.statementIds
-    }
