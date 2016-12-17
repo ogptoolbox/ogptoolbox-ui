2bf984f7b18c28182bbefb83512309cf927f137e
Emmanuel Raviart <emmanuel@raviart.com>
2016-12-17 23:43:36 +0100

    Remove special index-prerender.html.

--- webpack.config.js
+++ webpack.config.js
@@ -41,11 +41,6 @@ var commonConfig = {
       template: 'static/index.html',
       inject: 'body',
       filename: 'index.html'
-    }),
-    new HtmlWebpackPlugin({
-      template: 'static/index-prerender.html',
-      inject: 'body',
-      filename: 'index-prerender.html'
     })
   ],
   postcss: [ autoprefixer( { browsers: ['last 2 versions'] } ) ],
